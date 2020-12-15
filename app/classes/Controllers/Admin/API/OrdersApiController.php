ece19ca8e5774b3449fdb450c57f6ceb1f7cb385
tadasgodl <71317716+tadasgodl@users.noreply.github.com>
2020-12-15 10:25:28 +0200

    Added ApiControllers to Guest and Admin

---
+++ app/classes/Controllers/Admin/API/OrdersApiController.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Controllers\Admin\API;
+
+
+use App\Controllers\Base\AdminController;
+
+class OrdersApiController extends AdminController
+{
+    public function index() {
+
+    }
+
+    public function edit() {
+
+    }
+
+    public function update() {
+
+    }
+
+    public function delete() {
+
+    }
+}
---
+++ app/classes/Controllers/User/API/OrdersApiController.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Controllers\User\API;
+
+
+use App\Controllers\Base\API\UserController;
+
+class OrdersApiController extends UserController
+{
+
+}
